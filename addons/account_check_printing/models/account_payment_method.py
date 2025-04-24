ebded5b192dd9038622ac859a1db4187a95d3dcf
aizu-odoo <aizu@odoo.com>
2025-04-24 17:36:24 +0200

    [IMP] account*, payment*, l10n*: account payment methods refactoring
    Since 18.0, payments do not always create a payment entry. This creates a couple of side-effects impacting the meaning of what payments are across the application. While payments with outstanding account and journal entries have a meaning in terms of accounting, payments without journal entries are just a business indication that a payment will occur. 
    
    The following changes have been made:
    
    -Journals are no longer mandatory on the payment form view and the invoice payment wizard.
    
    -Inbound/outbound payment tabs on the journal are removed, the payment method line model is completely removed.
    
    -The outstanding payment account will now be set on the journal and only journals with this account set will appear on the payment forms/wizards. When set, if the account was not reconcilable, it will be set to reconcilable.
    
    -Payment method settings that used to be on the inbound/outbound tabs will stay on the journal advanced settings as these particular payment methods will still require a journal.
    
    -Payment providers with the same code will now be linked to a unique payment method.
    
    -Partners' preferred payment methods are now chosen from the payment methods instead of the payment method lines.
    
    [work in progress...]
    
    enterprise/pull/84052/files
    upgrade/pull/7614
    
    task-4655101

--- addons/account_check_printing/models/account_payment_method.py
+++ addons/account_check_printing/models/account_payment_method.py
@@ -1,14 +1,52 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class AccountPaymentMethod(models.Model):
     _inherit = 'account.payment.method'
 
+    check_manual_sequencing = fields.Boolean(
+        string='Manual Numbering',
+        default=False,
+        help="Check this option if your pre-printed checks are not numbered.",
+    )
+    check_sequence_id = fields.Many2one(
+        comodel_name='ir.sequence',
+        string='Check Sequence',
+        readonly=True,
+        copy=False,
+        help="Checks numbering sequence.",
+    )
+    check_next_number = fields.Char(
+        string='Next Check Number',
+        compute='_compute_check_next_number',
+        inverse='_inverse_check_next_number',
+        help="Sequence number of the next printed check.",
+    )
+
+    bank_check_printing_layout = fields.Selection(
+        selection='_get_check_printing_layouts',
+        string="Check Layout",
+    )
+
     @api.model
     def _get_payment_method_information(self):
         res = super()._get_payment_method_information()
-        res['check_printing'] = {'mode': 'multi', 'type': ('bank',)}
+        res['check_printing'] = {'type': ('bank',)}
         return res
+
+    def _get_check_printing_layouts(self):
+        """ Returns available check printing layouts for the company, excluding disabled options """
+        selection = self.company_id._fields['account_check_printing_layout'].selection
+        return [(value, label) for value, label in selection if value != 'disabled']
+
+    @api.depends('check_manual_sequencing')
+    def _compute_check_next_number(self):
+        for method in self:
+            sequence = method.check_sequence_id
+            if sequence:
+                method.check_next_number = sequence.get_next_char(sequence.number_next_actual)
+            else:
+                method.check_next_number = 1
