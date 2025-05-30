8ad08765d18a115148c0e07f0a2bd182d11b9355
Arib Ansari <aans@odoo.com>
2025-05-30 11:39:21 +0530

    [IMP] website: adapt snippet_translation_changing_lang tour
    `snippet_translation_changing_lang` tour was previously broken due to
    DOM structure changes introduced by the new website builder and was
    consequently disabled.
    
    This commit updates the tour steps to align with the new DOM and
    re-enables the associated test.

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -249,7 +249,6 @@ class TestUiTranslate(odoo.tests.HttpCase):
 
         self.start_tour(self.env['website'].get_client_action_url('/'), 'translate_text_options', login='admin')
 
-    @unittest.skip
     def test_snippet_translation(self):
         ResLang = self.env['res.lang']
         parseltongue, fake_user_lang = ResLang.create([{
