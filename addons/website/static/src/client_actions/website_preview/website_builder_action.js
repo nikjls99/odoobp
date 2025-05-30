364abc50bfb33646df60102310e28161a838fef3
Arib Ansari <aans@odoo.com>
2025-05-30 19:25:55 +0530

    [FIX] *: snippets not translating to the website's default language
    * = html_builder, website
    
    When adding snippets in Edit mode, they were always in English, even if
    the website's default language was different.
    
    Steps to reproduce:
    - Add a second language (e.g., French)
    - Set it as the website's default language
    - Enter Edit mode and add a snippet (e.g., s_cover)
    - Snippet content appears in English instead of the default language
    
    Root cause:
    We were loading the snippet service before the user enters the edit mode
    to ensure that the Edit mode loads faster.
    However, snippet service requires iframe to be ready to get the
    website's default language.
    This caused the snippet service to always load in English, as it never
    received the website's default language.
    
    Fix:
    This commit resolves the issue by ensuring that snippet service is only
    loaded after the iframe is ready.
    This ensures that snippets are still preloaded for faster Edit mode
    loading, but they are now in the website's default language.

--- addons/website/static/src/client_actions/website_preview/website_builder_action.js
+++ addons/website/static/src/client_actions/website_preview/website_builder_action.js
@@ -115,12 +115,6 @@ export class WebsiteBuilder extends Component {
             if (edition) {
                 this.onEditPage();
             }
-            if (!this.ui.isSmall) {
-                // preload builder and snippets so clicking on "edit" is faster
-                loadBundle("html_builder.assets").then(() => {
-                    this.env.services["html_builder.snippets"].load();
-                });
-            }
         });
         this.publicRootReady = new Deferred();
         this.setIframeLoaded();
@@ -274,6 +268,13 @@ export class WebsiteBuilder extends Component {
         if (this.props.action.context.params?.with_loader) {
             this.websiteService.hideLoader();
             this.props.action.context.params.with_loader = false;
+        }
+
+        if (!this.ui.isSmall) {
+            // Preload the snippets so clicking on "Edit" is faster
+            loadBundle("html_builder.assets").then(() => {
+                this.env.services["html_builder.snippets"].load();
+            });
         }
     }
 
