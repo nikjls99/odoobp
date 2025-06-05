odoo/odoo

[IMP] website_sale: submit form through main website checkout button
Use the main website checkout button to submit the form rendered on the side template.
extra_form.js submits the form that is selected on the extensions.
Ex: in extra_info.js, the selector links which form is gonna be submitted.

Remove _cta_classes and _form_send_navigation parameters as not used anymore.

addons/website_sale/static/src/js/extra_form.js
@@ -1,0 +1,37 @@
+import { Interaction } from "@web/public/interaction";
+
+
+export class WebsiteSaleFormButton extends Interaction {
+    dynamicSelectors = {
+        ...this.dynamicSelectors,
+        _submitbutton: () => document.querySelector('[name="website_sale_main_button"]'),
+    };
+    dynamicContent = {
+        _submitbutton: {"t-on-click": this.onSaveForm},
+    };
+
+    /**
+     * @param {Event} ev
+     * TODO-PDA simpler version with _submitForm see payment_form.js
+     */
+    onSaveForm(ev) {
+        const theForm = this.el;
+        const submitButton = ev.currentTarget;
+
+        if (!ev.defaultPrevented && !submitButton.disabled) {
+            ev.preventDefault();
+            submitButton.disabled = true;
+            const spinner = document.createElement('span');
+            spinner.classList.add('fa', 'fa-cog', 'fa-spin');
+            submitButton.appendChild(spinner);
+
+            const inputEl = document.createElement("input");
+            inputEl.setAttribute("type", "hidden");
+            inputEl.setAttribute("name", "csrf_token");
+            inputEl.setAttribute("value", odoo.csrf_token);
+            this.insert(inputEl, theForm);
+
+            theForm.submit();
+        }
+    }
+}

addons/website_sale/static/src/js/extra_info.js
@@ -1,0 +1,8 @@
+import { WebsiteSaleFormButton } from "@website_sale/js/form_button";
+import { registry } from "@web/core/registry";
+
+
+export class WebsiteSaleExtraInfoButton extends WebsiteSaleFormButton {
+    static selector = "#form_extra_info";
+}
+registry.category("public.interactions").add("website_sale.extra_info", WebsiteSaleExtraInfoButton);

nikjls99/odoobp

addons/website_sale/static/src/js/extra_form.js
@@ -1,0 +1,36 @@
+import { Interaction } from "@web/public/interaction";
+
+
+export class WebsiteSaleExtraForm extends Interaction {
+    dynamicSelectors = {
+        ...this.dynamicSelectors,
+        _submitbutton: () => document.querySelector('[name="website_sale_main_button"]'),
+    };
+    dynamicContent = {
+        _submitbutton: {"t-on-click": this.onSaveForm},
+    };
+
+    /**
+     * @param {Event} ev
+     */
+    onSaveForm(ev) {
+        const theForm = this.el;
+        const submitButton = ev.currentTarget;
+
+        if (!ev.defaultPrevented && !submitButton.disabled) {
+            ev.preventDefault();
+            submitButton.disabled = true;
+            const spinner = document.createElement('span');
+            spinner.classList.add('fa', 'fa-cog', 'fa-spin');
+            submitButton.appendChild(spinner);
+
+            const inputEl = document.createElement("input");
+            inputEl.setAttribute("type", "hidden");
+            inputEl.setAttribute("name", "csrf_token");
+            inputEl.setAttribute("value", odoo.csrf_token);
+            this.insert(inputEl, theForm);
+
+            theForm.submit();
+        }
+    }
+}

addons/website_sale/static/src/js/extra_info.js
@@ -1,0 +1,8 @@
+import { WebsiteSaleExtraForm } from "@website_sale/js/extra_form";
+import { registry } from "@web/core/registry";
+
+
+export class WebsiteSaleExtraInfoButton extends WebsiteSaleExtraForm {
+    static selector = "#form_extra_info";
+}
+registry.category("public.interactions").add("website_sale.extra_info", WebsiteSaleExtraInfoButton);